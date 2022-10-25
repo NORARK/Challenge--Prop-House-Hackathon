52b780deb08c6d7bc32c19bb3fc30cb2c0b5865e
NORARK <norark0x@gmail.com>
2022-10-26 02:57:49 +0900

    Fix.

---
+++ docs/_next/static/1RdN_di4sfKV0rNlPxBPY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6e6a5a85a6d4e096.css","static/chunks/pages/index-ee68aa86ee49023e.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
